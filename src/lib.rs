814ef2a02bd5104499c4163a06ea1f4738c58dc9
cmcpeek17 <cmcpeek17@gmail.com>
2026-02-01 13:56:41 -0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -99,8 +99,8 @@ where
     }
 
     fn register_read(&mut self, reg: u8) -> Result<u8, SpiE> {
-        let mut block: [u8; 2] = [reg | Self::DIR_READ; 2];
-        self.spi_dev.read( &mut block)?;
+        let mut block: [u8; 2] = [reg | Self::DIR_READ, 0u8];
+        self.spi_dev.transfer_in_place( &mut block)?;
 
         #[cfg(feature = "rttdebug")]
         rprintln!("read reg 0x{:x} {:x?} ", reg, block[1]);
